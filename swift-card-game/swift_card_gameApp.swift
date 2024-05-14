74e5b8343e54e5fffba0baecb77dfeedfd17d2b4
AndresCampuzano <campuxano88@gmail.com>
2024-05-14 07:22:37 -0500

    Initial Commit

---
+++ swift-card-game/swift_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_card_gameApp.swift
+//  swift-card-game
+//
+//  Created by Andres C G on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
